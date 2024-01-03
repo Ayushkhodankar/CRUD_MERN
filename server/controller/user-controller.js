8496a6d2dcab6b0449fc66424e5b62ff4ead24ea
Ayush Khodankar <ayushkhodankar01@gmail.com>
2024-01-04 04:35:11 +0530

    initial commit

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,13 +1,13 @@
 import User from "../schema/user-schema.js";
 
-export const addUser=async(request, response)=>{
-    const user= request.body;
-    const newUser= new User(user);
+export const addUser = async (request, response) => {
+    const user = request.body;
+    const newUser = new User(user);
 
     try {
-     await newUser.save();  
-      response.status(201).json(newUser);
+        await newUser.save();
+        response.status(201).json(newUser);
     } catch (error) {
-        response.status(400).json(error);
+        response.status(400).json({ message: error.message });
     }
 }
